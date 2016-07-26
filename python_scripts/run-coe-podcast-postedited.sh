c5d5797e0fb427ad493b15e5eaa2df0e4e3f493f
arendu <adithya.r@gmail.com>
2016-07-26 12:32:04 -0400

    updated pipeline scripts

--- python_scripts/run-coe-podcast-postedited.sh
+++ python_scripts/run-coe-podcast-postedited.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 set -e
 FOLDER="../data/de/nachrichtenleicht-podcast"
-FILE="nach-podcast-news.de"
-SPLIT_FILES_LOCATION="../data/de/tt/"
-./coe-from-mt.py -i $FOLDER/$FILE.tok -o $FOLDER/$FILE.wa.untangled.true.aligned.post -e $FOLDER/$FILE.tok.edges.uniq.intermediate -p $FOLDER/$FILE.tok.dep.parsed   --out  $FOLDER/$FILE
-./split_final_json.py -j  $FOLDER/$FILE.json -t  $FOLDER/$FILE.tok -o $SPLIT_FILES_LOCATION 
-echo "$FILE.json saved in '$FOLDER'"
-echo "split files saved in " $SPLIT_FILES_LOCATION
+for CAT in 'news' 'culture' 'sport'
+do
+    FILE="nach-podcast-$CAT.de"
+    SPLIT_FILES_LOCATION="../data/de/tt/"
+    ./insert_truecased.py -t  $FOLDER/$FILE.truecased -w $FOLDER/$FILE.wa.untangled.true.aligned.post >  $FOLDER/$FILE.wa.untangled.true.aligned.post.truecased
+    ./coe-from-mt.py -i $FOLDER/$FILE.tok -o $FOLDER/$FILE.wa.untangled.true.aligned.post.truecased -e $FOLDER/$FILE.tok.edges.uniq.intermediate -p $FOLDER/$FILE.tok.dep.parsed   --out  $FOLDER/$FILE
+    ./split_final_json.py -j  $FOLDER/$FILE.json -t  $FOLDER/$FILE.tok -o $SPLIT_FILES_LOCATION -c $CAT
+    echo "$FILE.json saved in '$FOLDER'"
+    echo "split files saved in " $SPLIT_FILES_LOCATION
+done
 
--- python_scripts/run-coe-podcast.sh
+++ python_scripts/run-coe-podcast.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 set -e
 FOLDER="../data/de/nachrichtenleicht-podcast"
-FILE="nach-podcast-news.de"
-SPLIT_FILES_LOCATION="../data/de/tt/"
-./rich2wa.py -r $FOLDER/$FILE.output > $FOLDER/$FILE.wa
-./make_unsplit.py -s $FOLDER/$FILE > $FOLDER/$FILE.unsplit
-./un-preorder.py -o $FOLDER/$FILE.tok -i $FOLDER/$FILE.unsplit -a $FOLDER/$FILE.wa > $FOLDER/$FILE.alignment
-./untangle-wa.py -i $FOLDER/$FILE.unsplit -o $FOLDER/$FILE.wa > $FOLDER/$FILE.wa.untangled
-./true-align.py -w $FOLDER/$FILE.wa.untangled -a $FOLDER/$FILE.alignment > $FOLDER/$FILE.wa.untangled.true.aligned
-./coe-from-mt.py -i $FOLDER/$FILE.tok -o $FOLDER/$FILE.wa.untangled.true.aligned -e $FOLDER/$FILE.tok.edges.uniq.intermediate -p $FOLDER/$FILE.tok.dep.parsed   --out  $FOLDER/$FILE
-./split_final_json.py -j  $FOLDER/$FILE.json -t  $FOLDER/$FILE.tok -o $SPLIT_FILES_LOCATION 
-echo "$FILE.json saved in '$FOLDER'"
-echo "split files saved in " $SPLIT_FILES_LOCATION
+for CAT in 'news' 'culture' 'sport'
+do
+    FILE="nach-podcast-$CAT.de"
+    SPLIT_FILES_LOCATION="../data/de/tt/"
+    ./rich2wa.py -r $FOLDER/$FILE.output > $FOLDER/$FILE.wa
+    ./make_unsplit.py -s $FOLDER/$FILE > $FOLDER/$FILE.unsplit
+    ./un-preorder.py -o $FOLDER/$FILE.tok -i $FOLDER/$FILE.unsplit -a $FOLDER/$FILE.wa > $FOLDER/$FILE.alignment
+    ./untangle-wa.py -i $FOLDER/$FILE.unsplit -o $FOLDER/$FILE.wa > $FOLDER/$FILE.wa.untangled
+    ./true-align.py -w $FOLDER/$FILE.wa.untangled -a $FOLDER/$FILE.alignment > $FOLDER/$FILE.wa.untangled.true.aligned
+    ./coe-from-mt.py -i $FOLDER/$FILE.tok -o $FOLDER/$FILE.wa.untangled.true.aligned -e $FOLDER/$FILE.tok.edges.uniq.intermediate -p $FOLDER/$FILE.tok.dep.parsed   --out  $FOLDER/$FILE
+    echo "$FILE.json saved in '$FOLDER'"
+    echo "split files saved in " $SPLIT_FILES_LOCATION
+done
 
